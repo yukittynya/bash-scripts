49682c3224955dc3f058864a36c2429e70aa9cf7
yukii <lichtestv@gmail.com>
2025-05-01 09:48:13 +0200

    Added scripts

--- dotfiles/selectWallpaper.sh
+++ dotfiles/selectWallpaper.sh
@@ -5,5 +5,12 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
-swww img $1
+FPS=60
+TYPE="any"
+DURATION=2
+BEZIER=".43,1.19,1,.4"
+SWWW_PARAMS="--transition-fps $FPS --transition-type $TYPE --transition-duration $DURATION --transition-bezier $BEZIER"
+
+swww img $1 $SWWW_PARAMS
 wallust run $1
+swaync-client -rs
